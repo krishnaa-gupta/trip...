0b67bb50b7e0de40589c0af3ec81a3b826be3d3b
Krishna Gupta <krishnaanew20@gmail.com>
2026-03-01 11:16:30 +0530

    hello

--- src/main/java/mytrip/mytrip/Railways.java
+++ src/main/java/mytrip/mytrip/Railways.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Railways {
 
     @GetMapping("/Railways")
-    public String getData() {return "Please book train from vande bharat at 25% discount"; }
+    public String getData() {return "Please book train from kashmir at 25% discount"; }
 
 }
